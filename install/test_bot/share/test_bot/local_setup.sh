b554cd9e7bfd88b704883fc1f267c1cd70e42dc7
indraAraujo <indra.araujo.santos@gmail.com>
2023-09-18 17:17:02 -0300

    ros2 launch

--- install/test_bot/share/test_bot/local_setup.sh
+++ install/test_bot/share/test_bot/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/indra/atmosBot_simulation/src/install/my_robot"}
+: ${AMENT_CURRENT_PREFIX:="/home/indra/atmosBot_simulation/install/test_bot"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
@@ -151,8 +151,8 @@ if [ -z "$AMENT_RETURN_ENVIRONMENT_HOOKS" ]; then
 fi
 
 # list all environment hooks of this package
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/my_robot/environment/ament_prefix_path.sh"
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/my_robot/environment/path.sh"
+ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/test_bot/environment/ament_prefix_path.sh"
+ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/test_bot/environment/path.sh"
 
 # source all shell-specific environment hooks of this package
 # if not returning them
