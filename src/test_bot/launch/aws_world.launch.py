570a91e77bc9ce06c06fc7bc32713d14259d8994
indraAraujo <indra.araujo.santos@gmail.com>
2023-10-20 20:19:51 -0300

    adicionado execução de rviz ao rodar

--- src/test_bot/launch/aws_world.launch.py
+++ src/test_bot/launch/aws_world.launch.py
@@ -15,6 +15,9 @@ def generate_launch_description():
   # Set the path to this package.
   pkg_share = FindPackageShare(package='test_bot').find('test_bot')
  
+  default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+  default_model_path = os.path.join(pkg_share, 'models/mobile_bot.urdf')
+
   # Set the path to the world file
   world_file_name = 'aws_house_bot.world'
   world_path = os.path.join(pkg_share, 'worlds', world_file_name)
@@ -29,7 +32,11 @@ def generate_launch_description():
   use_sim_time = LaunchConfiguration('use_sim_time')
   use_simulator = LaunchConfiguration('use_simulator')
   world = LaunchConfiguration('world')
- 
+  model = LaunchConfiguration('model')
+  use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+  rviz_config_file = LaunchConfiguration('rviz_config_file')
+  use_rviz = LaunchConfiguration('use_rviz')
+
   declare_simulator_cmd = DeclareLaunchArgument(
     name='headless',
     default_value='False',
@@ -49,7 +56,26 @@ def generate_launch_description():
     name='world',
     default_value=world_path,
     description='Full path to the world model file to load')
-    
+  
+  declare_model_path_cmd = DeclareLaunchArgument(
+    name='model', 
+    default_value=default_model_path, 
+    description='Absolute path to robot urdf file')
+  
+  declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+    name='use_robot_state_pub',
+    default_value='True',
+    description='Whether to start the robot state publisher')
+  
+  declare_rviz_config_file_cmd = DeclareLaunchArgument(
+    name='rviz_config_file',
+    default_value=default_rviz_config_path,
+    description='Full path to the RVIZ config file to use')
+
+  declare_use_rviz_cmd = DeclareLaunchArgument(
+    name='use_rviz',
+    default_value='True',
+    description='Whether to start RVIZ')
   # Specify the actions
    
   # Start Gazebo server
@@ -63,6 +89,25 @@ def generate_launch_description():
     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
     condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
  
+
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+  start_robot_state_publisher_cmd = Node(
+    condition=IfCondition(use_robot_state_pub),
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': use_sim_time, 
+    'robot_description': Command(['xacro ', model])}],
+    arguments=[default_model_path])
+
+  # Launch RViz
+  start_rviz_cmd = Node(
+    condition=IfCondition(use_rviz),
+    package='rviz2',
+    executable='rviz2',
+    name='rviz2',
+    output='screen',
+    arguments=['-d', rviz_config_file])  
+  
   # Create the launch description and populate
   ld = LaunchDescription()
  
@@ -71,9 +116,15 @@ def generate_launch_description():
   ld.add_action(declare_use_sim_time_cmd)
   ld.add_action(declare_use_simulator_cmd)
   ld.add_action(declare_world_cmd)
+  ld.add_action(declare_model_path_cmd)
+  ld.add_action(declare_use_robot_state_pub_cmd)  
+  ld.add_action(declare_rviz_config_file_cmd)
+  ld.add_action(declare_use_rviz_cmd) 
  
   # Add any actions
   ld.add_action(start_gazebo_server_cmd)
   ld.add_action(start_gazebo_client_cmd)
+  ld.add_action(start_robot_state_publisher_cmd)
+  ld.add_action(start_rviz_cmd)
  
   return ld
